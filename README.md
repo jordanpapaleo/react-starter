74f33d354174159e8567d7e8a30550d64d367d82
Jordan Papaleo <papaleowebdev@gmail.com>
2015-11-02 10:13:47 -0800

    Let there be code

---
+++ README.md
@@ -0,0 +1,9 @@
+# React Starter
+
+## Scripts
+- `npm run dev` - Starts the webpack server with hot reloading for development
+- `npm run test` - Lints the project files
+- `npm run dist` - Deletes the dist directory then re compiles the project into the dist directory
+
+
+[![js-standard-style](https://cdn.rawgit.com/feross/standard/master/badge.svg)](https://github.com/feross/standard)
---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React, { Component, PropTypes } from 'react'
+
+class App extends Component {
+  static get displayName () {
+    return ''
+  }
+
+  static get propTypes () {
+    return {
+
+    }
+  }
+
+  static get defaultProps () {
+    return {
+
+    }
+  }
+
+  constructor (props) {
+    super(props)
+  }
+
+  render () {
+    return (
+      <div>App</div>
+    )
+  }
+}
+
+export default App
